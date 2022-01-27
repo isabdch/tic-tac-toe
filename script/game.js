554cdd33091c92a32e82da833f292233132ff5cc
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2022-01-27 11:33:21 -0300

    update

--- script/game.js
+++ script/game.js
@@ -3,4 +3,13 @@ let board = ["", "", "", "", "", "", "", "", ""];
 let playerTime = 0;
 let symbols = ["o", "x"];
 
+// functions
+function handleMove(position) {
+  board[position] = symbols[playerTime];
 
+  if (playerTime == 0) {
+    playerTime = 1;
+  } else {
+    playerTime = 0;
+  }
+}
